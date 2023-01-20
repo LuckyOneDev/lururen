ce18f4130434d3a631789798fc0b391d3e31b874
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2023-01-21 01:18:47 +0300

    Added simple DllLoader

---
+++ Lururen.Core/ResourceLoader/DllResourceLoader/DllResourceLoaderBuilder.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lururen.Core.ResourceLoader.DllResourceLoader
+{
+    /// <summary>
+    /// Class to create loader. Can be used if there will ever be a need of more complex steps
+    /// </summary>
+    public class DllResourceLoaderBuilder
+    {
+        private readonly List<string> dllPaths = new();
+        public void AddDllToBeLoaded(string fullPath)
+        {
+            dllPaths.Add(fullPath);
+        }
+        public void AddDllToBeLoaded(string[] paths)
+        {
+            dllPaths.AddRange(paths);
+        }
+        public DllResourceLoader Build()
+        {
+            DllResourceLoader loader = new();
+            // Can be more steps here
+            loader.LoadDll(dllPaths.ToArray());
+            return loader;
+        }
+    }
+}
