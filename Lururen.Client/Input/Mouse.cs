5dd0e4e12874b965e03c90dfdcdea76571c944f1
Ivan Mokshin <imokshin2003@gmail.com>
2023-03-30 19:59:46 +0300

    Exposed mouse methods to user

--- Lururen.Client/Input/Mouse.cs
+++ Lururen.Client/Input/Mouse.cs
@@ -18,6 +18,13 @@ namespace Lururen.Client.Input
         /// <returns>Mouse position.</returns>
         public Vector2 Position => Window.MousePosition;
 
+        public bool IsButtonDown(MouseButton button) => Window.IsMouseButtonDown(button);
+        public bool IsButtonPressed(MouseButton button) => Window.IsMouseButtonPressed(button);
+        public bool IsButtonReleased(MouseButton button) => Window.IsMouseButtonReleased(button);
+        public bool IsAnyButtonDown() => Window.IsAnyMouseButtonDown;
+        public Vector2 Delta => Window.MouseState.Delta;
+        public Vector2 ScrollDelta => Window.MouseState.ScrollDelta;
+
         public void SetMouseCursorMode(CursorModeValue mode)
         {
             unsafe
