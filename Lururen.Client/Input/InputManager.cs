63b9a029154de098f87af4d67dabae30db85a9ff
Ivan Mokshin <imokshin2003@gmail.com>
2023-03-30 19:22:47 +0300

    Updated input manager

--- Lururen.Client/Input/InputManager.cs
+++ Lururen.Client/Input/InputManager.cs
@@ -6,22 +6,43 @@ namespace Lururen.Client.Input
 {
     // For later use if we need keypress event
     public delegate void KeyPressEvent(Keys key);
+
+    /// <summary>
+    /// Provides access to input devices: mouse, keyboard, controller etc.
+    /// </summary>
     public class InputManager
     {
-        GLWindow Window { get; set; }
+        GLWindow Window { get; }
         public InputManager(GLWindow Window)
         {
             this.Window = Window;
         }
 
+        /// <summary>
+        /// Checks if key is down.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>true if key is down.</returns>
         public bool IsKeyDown(Keys key) => Window.KeyboardState.IsKeyDown(key);
-        public bool IsKeyReleased(Keys key) => Window.KeyboardState.IsKeyReleased(key);
-        public bool IsKeyPressed(Keys key) => Window.KeyboardState.IsKeyPressed(key);
-        public Vector2 GetMousePos() => Window.MousePosition;
-        //public Action<KeyboardKeyEventArgs> KeyDown => Window.KeyDown;
-        public void Update()
-        {
 
-        }
+        /// <summary>
+        /// Checks if key was released.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>true if key was released.</returns>
+        public bool IsKeyReleased(Keys key) => Window.KeyboardState.IsKeyReleased(key);
+
+        /// <summary>
+        /// Checks if key was pressed.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>true if key was pressed.</returns>
+        public bool IsKeyPressed(Keys key) => Window.KeyboardState.IsKeyPressed(key);
+
+        /// <summary>
+        /// Gets mouse position on window. Mouse is relative to upper-left corner.
+        /// </summary>
+        /// <returns>Mouse position.</returns>
+        public Vector2 GetMousePos() => Window.MousePosition;
     }
 }
